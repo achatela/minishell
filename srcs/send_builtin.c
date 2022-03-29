f48ea3aa4d939051bb9f78100135326802f389f0
ChrisJmz <christianjimenezgf@gmail.com>
2022-03-29 13:30:58 +0200

    test

--- srcs/send_builtin.c
+++ srcs/send_builtin.c
@@ -2,27 +2,27 @@
 
 static int	get_command(char *s)
 {
-	if (ft_strlen(s) == 4 && s[0] == 'e' && s[1] == 'c'
+	if (ft_strlen(s) == 4 && s[0] == 'e' && s[1] == 'c'		/* ECHO */
 		&& s[2] == 'h' && s[3] == 'o' && s[4] == '\0')
 		return (1);
-	if (ft_strlen(s) == 2 && s[0] == 'c' && s[1] == 'd'
+	if (ft_strlen(s) == 2 && s[0] == 'c' && s[1] == 'd'		/* CD */
 		&& s[2] == '\0')
 		return (2);
-	if (ft_strlen(s) == 3 && s[0] == 'p' && s[1] == 'w'
+	if (ft_strlen(s) == 3 && s[0] == 'p' && s[1] == 'w'		/* PWD */
 		&& s[2] == 'd' && s[3] == '\0')
 		return (3);
-	if (ft_strlen(s) == 6 && s[0] == 'e' && s[1] == 'x'
+	if (ft_strlen(s) == 6 && s[0] == 'e' && s[1] == 'x'		/* EXPORT */
 		&& s[2] == 'p' && s[3] == 'o' && s[4] == 'r'
 		&& s[5] == 't' && s[6] == '\0')
 		return (4);
-	if (ft_strlen(s) == 5 && s[0] == 'u' && s[1] == 'n'
+	if (ft_strlen(s) == 5 && s[0] == 'u' && s[1] == 'n'		/* UNSET */
 		&& s[2] == 's' && s[3] == 'e' && s[4] == 't'
 		&& s[5] == '\0')
 		return (5);
-	if (ft_strlen(s) == 3 && s[0] == 'e' && s[1] == 'n'
+	if (ft_strlen(s) == 3 && s[0] == 'e' && s[1] == 'n'		/* ENV */
 		&& s[2] == 'v' && s[3] == '\0')
 		return (6);
-	if (ft_strlen(s) == 4 && s[0] == 'e' && s[1] == 'x'
+	if (ft_strlen(s) == 4 && s[0] == 'e' && s[1] == 'x'		/* EXIT */
 		&& s[2] == 'i' && s[3] == 't' && s[4] == '\0')
 		return (7);
 	return (-1);
