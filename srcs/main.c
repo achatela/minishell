e560fb1fa8335f38464959eb414a12821658ad07
Alexis CHATELAIN <achatela@e2r7p13.clusters.42paris.fr>
2022-03-30 14:43:24 +0200

    g_env malloc

--- srcs/main.c
+++ srcs/main.c
@@ -2,6 +2,31 @@
 
 char	**g_env = NULL;
 
+static char	**init_env(char **env, int i, int j, int k)
+{
+	while (env[k] != 0)
+		k++;
+	g_env = malloc(sizeof(char *) * (k + 1));
+	if (!g_env)
+		return (NULL);
+	while (i < k)
+	{
+		g_env[i] = malloc(sizeof(char) * (ft_strlen(env[i]) + 1));
+		if (!g_env[i])
+			return (NULL);
+		j = 0;
+		while (env[i][j])
+		{
+			g_env[i][j] = env[i][j];
+			j++;
+		}
+		g_env[i][j] = '\0';
+		i++;
+	}
+	g_env[i] = 0;
+	return (g_env);
+}
+
 int	main(int argc, char **argv, char **env)
 {
 	char	**path;
@@ -9,7 +34,7 @@ int	main(int argc, char **argv, char **env)
 
 	(void)argc;
 	(void)argv;
-	g_env = env;
+	g_env = init_env(env, 0, 0, 0);
 	path = get_path(g_env, 0);
 	while (1 && path != NULL)
 	{
