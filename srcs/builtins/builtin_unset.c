2c36efc8089b4510db10a4f6de7242b9dbc91391
ChrisJmz <christianjimenezgf@gmail.com>
2022-03-31 16:02:02 +0000

    unset approximatif

--- srcs/builtins/builtin_unset.c
+++ srcs/builtins/builtin_unset.c
@@ -1,19 +1,53 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   builtin_unset.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: cmsjuice <cmsjuice@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/31 13:11:06 by cmsjuice          #+#    #+#             */
-/*   Updated: 2022/03/31 14:09:04 by cmsjuice         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "minishell.h"
+
+char	*search_env(char *str, int i, int j)
+{
+	char	*ret;
+
+	while (str[i] && str[i] != '=')
+		i++;
+	if (str[i] == '\0')
+		return (NULL);
+	ret = malloc(sizeof(char) * i + 2);
+	if (!ret)
+		return (NULL);
+	while (str[j] != '=')
+	{
+		ret[j] = str[j];
+		j++;
+	}
+	ret[j] = '\0';
+	return (ret);
+}
+
+int	check_name(char *str)
+{
+	int i;
+
+	i = -1;
+	if (!ft_isalpha(str[0]))
+		return (1);
+	while (str[++i])
+	{
+		if (ft_isalnum(str[i]) == 0)
+			return (1);
+	}
+	return (0);
+	
+}
 
 void    builtin_unset(char **env, char **cmds)
 {
-    
+	int i;
 
+	i= 1;
+	if (!cmds[1])
+		return ;
+	while (env[++i])
+	{
+		if (check_name(cmds[1]) == 1)
+			printf("bash: unset: `%s': not a valid identifier\n", cmds[1]);
+		else if (ft_strcmp(search_env(env[i], 0, 0), cmds[1]) == 0)
+			env[i] = '\0';
+	}
 }
